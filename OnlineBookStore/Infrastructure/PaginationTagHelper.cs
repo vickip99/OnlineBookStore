6ffb45608ca2f50ce85b3ef8b58eb1121321364d
vicky polanco <vickyp812@gmail.com>
2024-03-20 14:11:09 -0600

    changes

--- OnlineBookStore/Infrastructure/PaginationTagHelper.cs
+++ OnlineBookStore/Infrastructure/PaginationTagHelper.cs
@@ -23,6 +23,13 @@ namespace OnlineBookStore.Infrastructure
 
         public PaginationInfo PageModel { get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; } = String.Empty;
+        public string PageClassNormal { get; set; } = String.Empty;
+        public string PageClassSelected { get; set; } = String.Empty;
+
+
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null) 
@@ -35,6 +42,13 @@ namespace OnlineBookStore.Infrastructure
                 {
                     TagBuilder tag = new TagBuilder("a");
                     tag.Attributes["href"]= urlHelper.Action(PageAction, new { pageNum = i });
+
+                    if (PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
+
                     tag.InnerHtml.Append(i.ToString());
 
                     result.InnerHtml.AppendHtml(tag);
--- OnlineBookStore/Program.cs
+++ OnlineBookStore/Program.cs
@@ -30,8 +30,7 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute("pagination", "Projects/{pageNum}", new { controller = "Home", action="Index" });
+app.MapDefaultControllerRoute();
 
 app.Run();
